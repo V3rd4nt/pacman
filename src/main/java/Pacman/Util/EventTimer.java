595c2e28ee274013f94b25e933c7dfbd0255fd8b
V3rd4nt <armoredfist87@gmail.com>
2016-01-18 11:33:48 +0100

    testfälle erweitert, kleiner Umbau im hauptprogramm

--- src/main/java/Pacman/Util/EventTimer.java
+++ src/main/java/Pacman/Util/EventTimer.java
@@ -18,7 +18,7 @@ public class EventTimer extends Thread {
      * @param e
      */
     public EventTimer(int time, ElementHandler eh, ElementHandler eh2, Element e, boolean testing) {
-        if (testing) this.time = time*100;
+        if (testing) this.time = time*1000;
         else this.time = time*10;
         this.eh = eh;
         this.eh2 = eh2;
--- src/test/java/JunitTests.java
+++ src/test/java/JunitTests.java
@@ -1,11 +1,9 @@
 import Pacman.Elements.*;
-import Pacman.Util.Messages;
 import Pacman.Util.Movement;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
-import sun.jvm.hotspot.debugger.posix.elf.ELFSectionHeader;
 
 import static org.junit.Assert.*;
 
@@ -28,25 +26,24 @@ public class JunitTests  {
         level = new Level (pacman, new Lifes(3));
     }
 
-
-    public void test0_createMoreGhostsThanAllowed () {
+    @Test
+    public void test1_createMoreGhostsThanAllowed () {
         for (int i = 0; i < 4; i++) assertTrue(level.createElement("GHOST", Movement.createRandomPosition()));
         assertFalse(level.createElement("GHOST", Movement.createRandomPosition()));
         ((GhostHandler)level.getGhostHandler()).getElements().clear();
     }
-
     @Test
-    public void test1_createAnElement () {
+    public void test2_createAnElement () {
         assertTrue(level.createElement("CORN", Movement.createRandomPosition()));
     }
 
     @Test
-    public void test2_createAnUndefinedElement () {
+    public void test3_createAnUndefinedElement () {
         assertFalse(level.createElement("UNDEFINED", Movement.createRandomPosition()));
     }
 
     @Test
-    public void test3_createPositionOutsidePlayingField () {
+    public void test4_createPositionOutsidePlayingField () {
         int x = Position.getWIDTH()+10;
         int y = Position.getHEIGHT()+10;
         Position pos = pos (x, y);
@@ -61,54 +58,50 @@ public class JunitTests  {
     }
 
     @Test
-    public void test4_createMorePillsThanAllowed () {
+    public void test5_createMorePillsThanAllowed () {
         for (int i = 0; i < 2; i++) assertTrue(level.createElement("PILL" , "POWER", Movement.createRandomPosition()));
         assertFalse(level.createElement("PILL" , "POWER", Movement.createRandomPosition()));
     }
 
     @Test
-    public void test5_PacmanEatsGhost () {
-        ((PillHandler)level.getPillHandler()).setTestingState(true);
-        level.createElement("PILL", "POWER", pos (1,2));
-        pacman.setPosition(pos (1,2));
-        level.eat(pos (1,2));
-        level.createElement("GHOST", pos (2,2));
-        pacman.setPosition(pos (2,2));
-        level.eat(pos (2,2));
-        assertTrue(level.getLifes() == 3);
-    }
-
-
-    public void test6_PacmanGetsEatenByGhost () {
-        ((PillHandler)level.getPillHandler()).setTestingState(true);
-        level.createElement("GHOST", pos (2,2));
-        pacman.setPosition(pos (2,2));
-        level.eat(pos (2,2));
-        assertTrue(level.getLifes() == 2);
-    }
-
-    public void test7_PacmanEatsAllGhostsAndGetsBonus () {
+    public void test6_PacmanEatsAllGhostsAndGetsBonus () {
         ((PillHandler)level.getPillHandler()).setTestingState(true);
 
         level.createElement("PILL", "POWER", pos(1, 2));
         level.createElement("PILL", "POWER", pos(3, 3));
+        level.createElement("GHOST", pos(15, 15));
+        level.createElement("GHOST", pos(16, 15));
+        level.createElement("GHOST", pos(17, 15));
+        level.createElement("GHOST", pos(18, 15));
 
         pacman.setPosition(pos(1, 2));
         level.eat(pos(1, 2));
-        level.createElement("GHOST", pos(2, 2));
-        pacman.setPosition(pos(2, 2));
+
+        for(int x = 42; x > 1; x--) {
+            pacman.setPosition(pos(x, 2));
+        }
+        ((Element)((GhostHandler)level.getGhostHandler()).getElements().get(0)).setPosition(pos(2,2));
         level.eat(pos(2, 2));
-        level.createElement("GHOST", pos(2, 3));
-        pacman.setPosition(pos(2, 3));
+
+        for(int y = 43; y > 2; y--) {
+            pacman.setPosition(pos(2, y));
+        }
+        ((Element)((GhostHandler)level.getGhostHandler()).getElements().get(0)).setPosition(pos(2,3));
         level.eat(pos(2, 3));
 
         pacman.setPosition(pos(3, 3));
         level.eat(pos(3, 3));
-        level.createElement("GHOST", pos(3, 4));
-        pacman.setPosition(pos(3, 4));
+
+        for(int y = 44; y > 3; y--) {
+            pacman.setPosition(pos(3, y));
+        }
+        ((Element)((GhostHandler)level.getGhostHandler()).getElements().get(0)).setPosition(pos(3,4));
         level.eat(pos(3, 4));
-        level.createElement("GHOST", pos(4, 4));
-        pacman.setPosition(pos(4, 4));
+
+        for(int x = 44; x > 3; x--) {
+            pacman.setPosition(pos(x, 4));
+        }
+        ((Element)((GhostHandler)level.getGhostHandler()).getElements().get(0)).setPosition(pos(4,4));
         level.eat(pos(4, 4));
 
         ((GhostHandler)level.getGhostHandler()).getBonus();
@@ -116,7 +109,35 @@ public class JunitTests  {
     }
 
     @Test
-    public void test8_PacmanEatsAllCorns(){
+    public void test7_PacmanEatsGhost () {
+        ((PillHandler)level.getPillHandler()).setTestingState(true);
+        level.createElement("PILL", "POWER", pos (1,2));
+        level.createElement("GHOST", pos (2,3));
+
+        pacman.setPosition(pos (1,2));
+        level.eat(pos (1,2));
+
+        for(int x = 42; x > 1; x--) {
+            pacman.setPosition(pos(x, 2));
+        }
+        ((Element)((GhostHandler)level.getGhostHandler()).getElements().get(0)).setPosition(pos(2,2));
+        level.eat(pos(2, 2));
+        assertTrue(level.getLifes() == 3);
+    }
+
+    @Test
+    public void test8_PacmanGetsEatenByGhost () {
+        ((PillHandler)level.getPillHandler()).setTestingState(true);
+        level.createElement("GHOST", pos (2,2));
+
+        pacman.setPosition(pos (2,2));
+        level.eat(pos (2,2));
+
+        assertTrue(level.getLifes() == 2);
+    }
+
+    @Test
+    public void test9_PacmanEatsAllCorns(){
         level.setAllCorns();
         int row = 0;
         int col = 0;
@@ -134,7 +155,6 @@ public class JunitTests  {
             }
             col++;
         }
-
         assertTrue(level.getCornHandler().getElements().isEmpty());
     }
 }
